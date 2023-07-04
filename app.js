68ec60c60cf030b53fc2839f9a35efe845c33aad
lipamaoa <filipa.ferreirasilva@gmail.com>
2023-07-04 18:00:44 +0100

    First commit

--- app.js
+++ app.js
@@ -19,4 +19,48 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers', { layout: 'layout', beers: beersFromApi });
+    })
+    .catch(error => {
+      console.log(error);
+      res.render('error', { error: 'Failed to fectch beers' });
+    });
+});
+
+app.get('/beers/:id', (req, res, next) => {
+  const beerId = req.params.id;
+
+  punkAPI
+    .getBeer(beerId)
+    .then(beer => {
+      res.render('partials/beer', { layout: 'layout', beer: beer[0] });
+    })
+    .catch(error => {
+      console.log(error);
+      res.render('error', { error: 'Failed to fetch beer' });
+    });
+});
+
+
+
+app.get('/random-beer', (req, res, next) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromApi => {
+      const beer = responseFromApi[0];
+      res.render('random-beer', {
+        layout: 'layout',
+        beer: beer
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      res.render('error', { error: 'Failed to fectch beers' });
+    });
+});
+
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
